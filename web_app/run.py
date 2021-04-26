902b1113ddda9a2409bf2cba504b8b74483081cd
Nikita <danila.nikitamihai@gmail.com>
2021-04-26 21:09:23 +0300

    Login and Sign Up working, no styling

--- web_app/run.py
+++ web_app/run.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, Response
-from flask.helpers import url_for
+from flask import Flask, render_template, Response, url_for
 from forms import LoginForm, SignupForm
-# from hardware import Camera
-# import cv2
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e0c2e77c7caec2a08617ff7f2df59d2c'
 
@@ -16,7 +14,7 @@ def about():
 
 @app.route('/login')
 def login():
-    form = LoginForm
+    form = LoginForm()
     return render_template('login.html', title='Login', form=form)
 
 @app.route('/register')
@@ -24,22 +22,9 @@ def register():
     form = SignupForm()
     return render_template('register.html',title='Sign Up', form=form)
 
-# @app.route('/stream')
-# def stream():
-#     def gen(camera):
-#         while True:
-#             success, frame = camera.read()  # read the camera frame
-#             if not success:
-#                 break
-#             else:
-#                 ret, buffer = cv2.imencode('.jpg', frame)
-#                 frame = buffer.tobytes()
-#                 yield (b'--frame\r\n'
-#                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
-    
-#     return Response(gen(Camera.camera),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-#     # return render_template('stream.html', title='Stream')
+@app.route('/stream')
+def stream():
+    return render_template('stream.html', title='Stream')
 
 if __name__ == '__main__':
     app.run(debug=True)
