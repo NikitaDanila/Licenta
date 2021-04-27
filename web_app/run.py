9f5d8e6eebbc98988d2248d0894265d6fdf4e904
Nikita <danila.nikitamihai@gmail.com>
2021-04-27 17:11:28 +0300

    added db models and gitignore

--- web_app/run.py
+++ web_app/run.py
@@ -1,9 +1,40 @@
+from enum import unique
 from flask import Flask, render_template, Response, url_for, flash, redirect
 from forms import LoginForm, SignupForm
+from flask_sqlalchemy import SQLAlchemy
 
+# Creating aplication
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e0c2e77c7caec2a08617ff7f2df59d2c'
+# Database 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class Profesor(db.Model):
+    id_profesor = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    admin = db.Column(db.Boolean, nullable=True, default=1)
+
+    def __repr__(self):
+        return f"Profesor:\nid_profesor: {self.id_prosefor}\nfirst_name: {self.first_name}\nlast_name: {self.last_name}"
+
+
+class Elev(db.Model):
+    id_elev= db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True, nullable=False)
+    email = db.Column(db.String(30), nullable=False, unique=True)
+    first_name = db.Column(db.String(20), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    admin = db.Column(db.Boolean, nullable=False, default=0)
+
+    def __repr__(self):
+        return f"Elev:\nid_elev: {self.id_elev}\nfirst_name: {self.first_name}\nlast_name: {self.last_name}"
+
+
+# Web pages (routes)
 @app.route('/')
 def index():
     return render_template('index.html',title='index')
