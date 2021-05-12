237921552b2868008fb3b8d324c32b5522bbbf37
NikitaDanila <danila.nikitamihai@gmail.com>
2021-05-12 14:51:46 +0300

    Working on raspberry pi

--- web_app/web_app/__init__.py
+++ web_app/web_app/__init__.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e0c2e77c7caec2a08617ff7f2df59d2c'
 
 # Database 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///E:\Licenta\web_app\database\site_database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/pi/Licenta/web_app/database/site_database.db'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///E:\Licenta\web_app\database\site_database.db'
 db = SQLAlchemy(app)
 
 # Bcrypt
