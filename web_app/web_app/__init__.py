d581b672e399798c56631c70d8b5403eb690a3c7
Nikita <danila.nikitamihai@gmail.com>
2021-05-12 09:42:09 +0300

    added login manager. Moving to one table user

--- web_app/web_app/__init__.py
+++ web_app/web_app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from flask_login import LoginManager, login_manager
 # Creating aplication
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'e0c2e77c7caec2a08617ff7f2df59d2c'
@@ -11,3 +11,5 @@ db = SQLAlchemy(app)
 # Bcrypt
 bcrypt = Bcrypt()
 from web_app import routes
+# Login
+login_manager = LoginManager(app)
