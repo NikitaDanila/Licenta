7ef74613d002c8e00077ad73780ebacc74ba711c
Nikita <danila.nikitamihai@gmail.com>
2021-05-12 11:00:11 +0300

    navbar working

--- web_app/web_app/routes.py
+++ web_app/web_app/routes.py
@@ -2,7 +2,7 @@ from web_app import app, db, bcrypt
 from flask import render_template, url_for, flash, redirect
 from web_app.forms import LoginForm, SignupForm
 from database.models import User
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 """ Web pages (routes)"""
 
 @app.route('/')
@@ -14,11 +14,14 @@ def about():
     return render_template('about.html', title='About')
 @app.route('/login', methods=['GET','POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
             user = User.query.filter_by(email=form.email.data).first()
             if user and bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
+                flash(f'logged in as {form.email.data}')
                 return(redirect(url_for('index')))
             else:
                 flash('Login Unsuccessful. Please check emails and possword', 'danger')
@@ -27,6 +30,8 @@ def login():
 
 @app.route('/register', methods=['GET','POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = SignupForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -46,3 +51,8 @@ def register():
 @app.route('/stream')
 def stream():
     return render_template('stream.html', title='Stream')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
