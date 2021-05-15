32ef55479a009d16c225cc41b098c92bee724049
NikitaDanila <danila.nikitamihai@gmail.com>
2021-05-16 00:26:38 +0300

    added certificates

--- web_app/web_app/forms.py
+++ web_app/web_app/forms.py
@@ -5,17 +5,17 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 class SignupForm(FlaskForm):
     """Creates the form for the sign up page"""
     username = StringField('Username', 
-                            validators=[DataRequired(), Length(min=2, max=20, message='Boss mai lung un pic')])
+                            validators=[DataRequired(), Length(min=2, max=20, message='Please enter a valid Username')])
     first_name = StringField('First Name', 
-                            validators=[DataRequired(), Length(min=2, max=20)])
+                            validators=[DataRequired(), Length(min=2, max=20, message='Please enter a valid First Name')])
     last_name = StringField('Last Name', 
-                            validators=[DataRequired(), Length(min=2, max=20)])                          
+                            validators=[DataRequired(), Length(min=2, max=20, message='Please enter a valid Last Name')])                          
     email = StringField('Email',
-                        validators=[DataRequired(), Email(message='Not gut')])
+                        validators=[DataRequired(), Email(message='Please enter a valid Email')])
     password = PasswordField('Password',
-                            validators=[DataRequired(), Length(min=2, max=20, message='Boss mai lung un pic')])
+                            validators=[DataRequired(), Length(min=2, max=20, message='Please enter a valid Email')])
     confirm_password = PasswordField('Confirm Password',
-                            validators=[DataRequired(), Length(min=2, max=20, message='Boss mai lung un pic'), EqualTo('password')])
+                            validators=[DataRequired(), Length(min=2, max=20, message='Please enter a valid Password'), EqualTo('password', message='The passwords do not match')])
     profesor_token = PasswordField('Profesor token', validators=[Length(min=0,max=20)], default=0)
     submit = SubmitField('Sign Up')
 
@@ -23,9 +23,9 @@ class SignupForm(FlaskForm):
 class LoginForm(FlaskForm):
     """Creates the form for the login page"""
     email = StringField('Email',
-                        validators=[DataRequired(), Email(message='Boss mai lung un pic')])
+                        validators=[DataRequired(), Email(message='Please enter a valid Email')])
     password = PasswordField('Password',
-                            validators=[DataRequired()])
+                            validators=[DataRequired(message='Please enter a password')])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
