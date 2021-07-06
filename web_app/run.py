494e6cc21e6cb97d5cd000a3a915880d4f60052a
Nikita Danila <danila.nikitamihai@gmail.com>
2021-07-06 20:52:22 +0300

    good

--- web_app/run.py
+++ web_app/run.py
@@ -4,4 +4,4 @@ app.config['FLASK_APP']='run.py'
 app.config['FLASK_ENV']='development'
 app.config['SECRET_KEY'] = os.environ.get('WEB_APP_SECRET_KEY')
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
